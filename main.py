e54b771fa8e98ad9ac3204198b5bf1532310c8d4
Anton Irvold <40317988+antonvdi@users.noreply.github.com>
2020-04-05 23:07:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ green = (0, 255, 0)
 red = (255, 0, 0)
 
 all_objects = list()
+n = 0
 
 class Object:
-	def __init__(self, mass, size, position_vertical, position_horizontal):
+	def __init__(self, mass, size, position_vertical, position_horizontal, color):
 		self.mass = mass
 		self.size = size
 		self.force_horizontal = 0
@@ -29,16 +30,17 @@ class Object:
 		self.position_horizontal = position_horizontal
 		self.position_vertical = position_vertical
 		self.last_update = time.time()
+		self.color = color
 
 	def update(self):
-		delta_time = (time.time()-self.last_update)*7
+		delta_time = (time.time()-self.last_update) * 7
 		self.velocity_horizontal += self.acceleration_horizontal*delta_time
 		self.velocity_vertical += self.acceleration_vertical*delta_time
 		self.position_horizontal += self.velocity_horizontal*delta_time
 		self.position_vertical += self.velocity_vertical*delta_time
 		self.last_update = time.time()
 
-		pygame.draw.circle(screen, blue, (int(self.position_horizontal), int(self.position_vertical)), self.size)
+		pygame.draw.circle(screen, self.color, (int(self.position_horizontal), int(self.position_vertical)), self.size)
 
 	def wallcollision(self):
 		if self.position_vertical + self.size >= height:
@@ -59,8 +61,8 @@ class Object:
 
 def main():
 	done = False
-	object1 = Object(20, 20, 350, 350)
-	object2 = Object(40, 40, 100, 100)
+	object1 = Object(20, 20, 350, 350, blue)
+	object2 = Object(40, 40, 100, 100, red)
 
 	while not done:
 		for event in pygame.event.get():
@@ -73,6 +75,7 @@ def main():
 		object1.wallcollision()
 		object2.wallcollision()
 		collision_circle(object1, object2)
+		#collision_circle(object2, object1)
 		
 		
 		pygame.display.flip()
@@ -80,7 +83,7 @@ def main():
 def collision_circle(circle1, circle2):
 	#the vector in between the two circles, cartesian coordinates
 	position_vector_horizontal = circle2.position_horizontal - circle1.position_horizontal
-	position_vector_vertical = circle2.position_vertical - circle2.position_vertical
+	position_vector_vertical = circle2.position_vertical - circle1.position_vertical
 
 	#polar coordinates
 	position_vector_length = math.sqrt((position_vector_horizontal)**2+(position_vector_vertical)**2)
@@ -91,20 +94,16 @@ def collision_circle(circle1, circle2):
 		tangent_vector_horizontal = position_vector_horizontal
 		tangent_vector_vertical = -position_vector_vertical
 
+		tangent_vector_direction = math.atan(tangent_vector_vertical/tangent_vector_horizontal)
 		tangent_vector_length = math.sqrt(tangent_vector_horizontal**2+tangent_vector_vertical**2)
-		tangent_vector_direction = math.acos((tangent_vector_horizontal*position_vector_horizontal+tangent_vector_vertical*position_vector_vertical)/(position_vector_length*tangent_vector_length))
 
-		#tangent and velocity
-		approach_direction1 = (circle1.velocity_horizontal*tangent_vector_horizontal+circle1.velocity_vertical*tangent_vector_vertical)/tangent_vector_length*math.sqrt((circle1.velocity_vertical)**2+(circle1.velocity_horizontal)**2)
-		approach_direction2 = (circle2.velocity_horizontal*tangent_vector_horizontal+circle2.velocity_vertical*tangent_vector_vertical)/tangent_vector_length*math.sqrt((circle2.velocity_vertical)**2+(circle2.velocity_horizontal)**2) 
+		circle1_velocity = math.sqrt(circle1.velocity_horizontal**2+circle1.velocity_vertical**2)
 
-		reflection_direction1 = approach_direction1-2*math.pi
-		reflection_direction2 = approach_direction2-2*math.pi
+		approach_direction = math.acos((circle1.velocity_horizontal*tangent_vector_horizontal+circle1.velocity_vertical*tangent_vector_vertical)/(circle1_velocity*tangent_vector_length))
 
-		circle1.velocity_horizontal = math.sqrt(circle1.velocity_horizontal**2+circle1.velocity_vertical**2) * math.cos(reflection_direction1)
-		circle2.velocity_horizontal = math.sqrt(circle2.velocity_horizontal**2+circle2.velocity_vertical**2) * math.cos(reflection_direction1)
+		outgoing_direction = approach_direction + tangent_vector_direction
 
-		circle1.velocity_vertical = math.sqrt(circle1.velocity_horizontal**2+circle1.velocity_vertical**2) * math.sin(reflection_direction1)
-		circle2.velocity_vertical = math.sqrt(circle2.velocity_horizontal**2+circle2.velocity_vertical**2) * math.sin(reflection_direction1)
+		circle1.velocity_horizontal = circle1_velocity * math.cos(outgoing_direction)
+		circle1.velocity_vertical = circle1_velocity * math.sin(outgoing_direction)
 
 main()
